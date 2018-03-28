4862532a0e49f880a5092450327fa73e2838468a
liumapp <liumapp.com@gmail.com>
2018-03-28 14:38:07 +0800

    add 3 types of controller

--- tdd-service/demo-api-a/src/main/java/com/liumapp/demo/tdd/service/demoapia/controller/FruitController.java
+++ tdd-service/demo-api-a/src/main/java/com/liumapp/demo/tdd/service/demoapia/controller/FruitController.java
@@ -5,15 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author liumapp
- * @file CustomerController.java
+ * @file FruitController.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 3/28/18
  */
 @RestController
-@RequestMapping("customer")
-public class CustomerController {
-
-    
-
+@RequestMapping("fruit")
+public class FruitController {
 }
--- tdd-service/demo-api-a/src/main/java/com/liumapp/demo/tdd/service/demoapia/controller/OrderController.java
+++ tdd-service/demo-api-a/src/main/java/com/liumapp/demo/tdd/service/demoapia/controller/OrderController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author liumapp
- * @file CustomerController.java
+ * @file OrderController.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 3/28/18
  */
 @RestController
-@RequestMapping("customer")
-public class CustomerController {
+@RequestMapping("order")
+public class OrderController {
 
-    
+
 
 }
