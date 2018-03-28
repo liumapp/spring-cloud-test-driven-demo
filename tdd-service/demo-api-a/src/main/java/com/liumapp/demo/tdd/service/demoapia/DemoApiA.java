47894764bd52fffacc0920de9f49a81ff3f5ff3c
liumapp <liumapp.com@gmail.com>
2018-03-28 13:54:29 +0800

    import mybatis mapper

--- tdd-service/demo-api-a/src/main/java/com/liumapp/demo/tdd/service/demoapia/DemoApiA.java
+++ tdd-service/demo-api-a/src/main/java/com/liumapp/demo/tdd/service/demoapia/DemoApiA.java
@@ -3,6 +3,7 @@ package com.liumapp.demo.tdd.service.demoapia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author liumapp
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@Import(value = {com.liumapp.demo.tdd.engine.model.ModelConfig.class})
 public class DemoApiA {
 
     public static void main(String[] args) {
